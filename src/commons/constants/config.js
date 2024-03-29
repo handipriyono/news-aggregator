2e558a968cc82117c45e6051365cd7c249b39cd1
Handi Priyono <handimacbook@gmail.com>
2024-03-29 07:58:39 +0700

    feat: add constants file

---
+++ src/commons/constants/config.js
@@ -0,0 +1,7 @@
+export const NEWS_API = "https://newsapi.org";
+export const GUARDIAN_API = "https://content.guardianapis.com";
+export const NYTIMES_API = "https://api.nytimes.com";
+
+export const APIKEY_NEWS_API = "f946b86301da4fb18143089db8199873";
+export const APIKEY_GUARDIAN = "5aa420e3-b5f4-4feb-9c92-ff199d8a3a52";
+export const APIKEY_NYTIMES = "f675CGTaMddZRJUZWvAezlRWpptJ4AWa";
